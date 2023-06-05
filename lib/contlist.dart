08e2e9f5b8d489e4d2e873e9e2026536b857d3ec
NeerajM1337 <neerajm1711@gmail.com>
2023-06-05 12:49:04 +0530

    first commit

--- lib/contlist.dart
+++ lib/contlist.dart
@@ -1,254 +1,254 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class contlist extends StatelessWidget {
-  const contlist({Key? key}) : super(key: key);
+// class contlist extends StatelessWidget {
+//   const contlist({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
 
-      appBar: AppBar(
-        leading:Icon(Icons.arrow_back),
+//       appBar: AppBar(
+//         leading:Icon(Icons.arrow_back),
 
-        title: Text("List view with contact"),
-        backgroundColor: Colors.green[300],
+//         title: Text("List view with contact"),
+//         backgroundColor: Colors.green[300],
 
-      ),
+//       ),
 
-      body: ListView(
-          children: [
-      Container(
+//       body: ListView(
+//           children: [
+//       Container(
 
-        child:ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Colors.blue,
-          child: Icon(Icons.person_3_rounded),
-        ),
-      trailing: Icon(Icons.keyboard_arrow_right_outlined),
-      title: Text("CHJISHA CHINNU"),
-      subtitle: Text("9995566122"),
+//         child:ListTile(
+//         leading: CircleAvatar(
+//           backgroundColor: Colors.blue,
+//           child: Icon(Icons.person_3_rounded),
+//         ),
+//       trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//       title: Text("CHJISHA CHINNU"),
+//       subtitle: Text("9995566122"),
 
 
-      ),
+//       ),
 
-    ),
+//     ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.yellow,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("ANUSHA ANNU"),
-                subtitle: Text("8995566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.yellow,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("ANUSHA ANNU"),
+//                 subtitle: Text("8995566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
 
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.deepOrange,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("MANUSHA MINNU"),
-                subtitle: Text("7795566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.deepOrange,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("MANUSHA MINNU"),
+//                 subtitle: Text("7795566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("PALLI PALLUNNU"),
-                subtitle: Text("9445566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.green,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("PALLI PALLUNNU"),
+//                 subtitle: Text("9445566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
-
-            Container(
-
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("SALISHA INNU"),
-                subtitle: Text("6595566122"),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
+
+//             Container(
+
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.grey,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("SALISHA INNU"),
+//                 subtitle: Text("6595566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
-
-            Container(
-
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.redAccent,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("JOHNNY JOHN"),
-                subtitle: Text("9995566122"),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
+
+//             Container(
+
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.redAccent,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("JOHNNY JOHN"),
+//                 subtitle: Text("9995566122"),
 
 
-              ),
-
-            ),
+//               ),
+
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.yellow,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("SANUSHA SANNU"),
-                subtitle: Text("8995566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.yellow,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("SANUSHA SANNU"),
+//                 subtitle: Text("8995566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.deepOrange,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("VANUSHA VINNU"),
-                subtitle: Text("7795566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.deepOrange,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("VANUSHA VINNU"),
+//                 subtitle: Text("7795566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
+//             Container(
 
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("MALLI MALLU"),
-                subtitle: Text("9445566122"),
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.green,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("MALLI MALLU"),
+//                 subtitle: Text("9445566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
-            Container(
-              child:ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person_3_rounded),
-                ),
-                trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                title: Text("SALISHA INNU"),
-                subtitle: Text("6595566122"),
+//             Container(
+//               child:ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundColor: Colors.grey,
+//                   child: Icon(Icons.person_3_rounded),
+//                 ),
+//                 trailing: Icon(Icons.keyboard_arrow_right_outlined),
+//                 title: Text("SALISHA INNU"),
+//                 subtitle: Text("6595566122"),
 
 
-              ),
+//               ),
 
-            ),
+//             ),
 
-            Divider(                    // Add this Divider widget
-              color: Colors.grey,       // Specify the color of the line
-              thickness: 1,             // Specify the thickness of the line
-              indent: 16,               // Specify the indent (left padding) of the line
-              endIndent: 16,            // Specify the end indent (right padding) of the line
-            ),
+//             Divider(                    // Add this Divider widget
+//               color: Colors.grey,       // Specify the color of the line
+//               thickness: 1,             // Specify the thickness of the line
+//               indent: 16,               // Specify the indent (left padding) of the line
+//               endIndent: 16,            // Specify the end indent (right padding) of the line
+//             ),
 
 
-    ],
-    ),
+//     ],
+//     ),
 
 
-      );
+//       );
 
 
 
-  }
-}
+//   }
+// }
